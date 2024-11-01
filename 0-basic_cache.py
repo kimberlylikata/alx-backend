20e6d9048d0d3ede444dc5ded9ad5eecc91b351e
kimberlylikata <kimberleylikata@gmail.com>
2024-11-01 12:44:17 +0300

    caching

--- 0-basic_cache.py
+++ 0-basic_cache.py
@@ -1,15 +1,23 @@
-# 0-basic_cache.py
+#!/usr/bin/env python3
+'''0. Basic dictionary'''
 
-from base_caching import BaseCaching
+
+BaseCaching = __import__('base_caching').BaseCaching
+
 
 class BasicCache(BaseCaching):
-    """BasicCache class - A caching system without a limit"""
+    '''
+    BasicCache inherits from BaseCaching and extend it
+    '''
+    def __init__(self):
+        '''Initiate an instant'''
+        super().__init__()
 
     def put(self, key, item):
-        """Assigns the item value to the key in cache_data dictionary"""
+        '''assign item to key in cache_data'''
         if key is not None and item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Returns the value linked to the key in cache_data"""
-        return self.cache_data.get(key, None)
+        '''Return the value in self.cache_data linked to key or None'''
+        return self.cache_data.get(key)
